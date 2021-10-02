d82b03ed07028dae79eb4763e53360dfb68aafdd
timothy.ding <timothy.ding@wizvision.com>
2021-10-02 22:09:44 +0800

    Added get autosys job logic

--- ShibaReader/Controllers/MainWindowController.cs
+++ ShibaReader/Controllers/MainWindowController.cs
@@ -2,12 +2,17 @@
 using ShibaReader.Processors;
 using ShibaReader.Utils;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ShibaReader.Controllers
 {
     class MainWindowController
     {
         private Dictionary<string, AutoSysJob> autoSysJobs = null;
+        private Dictionary<string, List<string>> calendarDates = new Dictionary<string, List<string>>();
+        private List<string> matchedJobs = new List<string>();
+        private int currentIndex = 0;
+        private int increment = 1;
         public void importJILFile()
         {
             string fileName = FileUtils.openFileChooser();
@@ -15,6 +20,50 @@ namespace ShibaReader.Controllers
 
             JILProcessor jilProc = new JILProcessor(fileName);
             autoSysJobs = jilProc.processJILFile();
+        }
+
+        public void importCALFile()
+        {
+            string fileName = FileUtils.openFileChooser();
+            if (fileName == null) return;
+
+            CALProcessor calProc = new CALProcessor(fileName);
+
+        }
+
+        public AutoSysJob getCurrJob()
+        {
+            if (matchedJobs.Count == 0) return null;
+
+            var job = autoSysJobs[matchedJobs[currentIndex]];
+
+            if (matchedJobs.Count > 1)
+            {
+                if (currentIndex + increment >= matchedJobs.Count)
+                {
+                    increment = -1;
+                }
+                else if (currentIndex + increment < 0)
+                {
+                    increment = 1;
+                }
+                currentIndex += increment;
+            }
+            return job;
+        }
+
+        public AutoSysJob searchJob(string jobName)
+        {
+            if (autoSysJobs.ContainsKey(jobName))
+            {
+                matchedJobs.Clear();
+                matchedJobs.Add(jobName);
+            }
+            else
+            {
+                matchedJobs = autoSysJobs.Keys.Where(k => k.Contains(jobName)).ToList();
+            }
+            return getCurrJob();
         }
     }
 }
